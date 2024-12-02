d0280497b0e28ea20852aefb58ae27e3c0c51807
Ruchit <risenid@duck.com>
2024-12-02 15:14:44 +0530

    a72q: Adapt vendor fingerprints to A15 format
    Change-Id: I0dea6e497f924d7cd666cf12e4d59d5715895a63
    Signed-off-by: Ruchit <risenid@duck.com>

--- lineage_a72q.mk
+++ lineage_a72q.mk
@@ -44,8 +44,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Fingerprint
-BUILD_FINGERPRINT := "samsung/a72qnsxx/a72q:11/RP1A.200720.012/A725FXXSAFXJ1:user/release-keys"
-PRIVATE_BUILD_DESC := "a72qnsxx-user 11 RP1A.200720.012 A725FXXSAFXJ1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint="samsung/a72qnsxx/a72q:11/RP1A.200720.012/A725FXXSAFXJ1:user/release-keys" \
+	BuildDesc="a72qnsxx-user 11 RP1A.200720.012 A725FXXSAFXJ1 release-keys"
 
 # Security patch
 VENDOR_SECURITY_PATCH := 2024-05-01
